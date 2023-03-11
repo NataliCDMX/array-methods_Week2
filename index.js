9ed7ca677d01c57c91d211e84100cdfc8e3011c6
NataliCDMX <lili9106@gmail.com>
2023-03-11 21:24:18 +0300

    add

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const filterByAge = (el) => {
-
+    return el.age < 50;
 };
 
 const filterByScore = (el) => {
+    return el.score > 75;
 
 };
 
 const filterByServiceRecord = (el) => {
+    return el.serviceRecord > 5;
 
 };
 
@@ -15,6 +17,11 @@ const sortByScore = (arr) => {
 };
 
 const getMeanAge = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i].age;
+    }
+    return sum / arr.length;
 
 };
 
